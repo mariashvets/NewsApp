f52b8d4f50dacf7de4dff1740bfb330b974ec5e5
Maria Shvets <maria@shvets.eu>
2018-01-24 18:25:04 +0200

    initial commit

---
+++ src/ArticleList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Article from './Article';
+
+
+export default function ArticleList ({articles}) {
+
+    const elements = articles.map((article) => <li key={article.id}><Article  article={article}/></li>);
+    return (
+        <ul>{elements}</ul>
+    )
+}
+
+// export default class ArticleList extends Component {
+//     render() {
+//         const elements = articles.map((article) => <li key={article.id}><Article  article={article}/></li>);
+//         return (
+//             <ul>{elements}</ul>
+//         )
+//     }
+// }
---
+++ src/fixtures.js
@@ -0,0 +1,110 @@
+export const articles = [
+
+    {
+        "id": "178567486746",
+        "date": "23434324",
+        "title": "What is Lorem Ipsum?",
+        "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+        "comments": [
+            {
+                "id": "0",
+                "user": "0 Johny Bonny",
+                "text": "0 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "1",
+                "user": "1 Johny Bonny",
+                "text": "1 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "2",
+                "user": "2 Johny Bonny",
+                "text": "2 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "3",
+                "user": "3Johny Bonny",
+                "text": "3 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+
+
+
+        ]
+    },
+
+
+    {
+        "id": "5645656456456",
+        "date": "23434324",
+        "title": "Why do we use it?",
+        "text": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+
+        "comments": [
+            {
+                "id": "0",
+                "user": "0 Johny Bonny",
+                "text": "0 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "1",
+                "user": "1 Johny Bonny",
+                "text": "1 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "2",
+                "user": "2 Johny Bonny",
+                "text": "2 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "3",
+                "user": "3Johny Bonny",
+                "text": "3 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "4",
+                "user": "4Johny Bonny",
+                "text": "4 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+
+
+        ]
+    },
+
+
+    {
+        "id": "5654654654645",
+        "date": "23434324",
+        "title": "What is Lorem Ipsum?",
+        "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+        "comments": [
+            {
+                "id": "0",
+                "user": "0 Johny Bonny",
+                "text": "0 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "1",
+                "user": "1 Johny Bonny",
+                "text": "1 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "2",
+                "user": "2 Johny Bonny",
+                "text": "2 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                "id": "3",
+                "user": "3Johny Bonny",
+                "text": "3 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+
+
+
+        ]
+    },
+
+
+
+];
