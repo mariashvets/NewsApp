a645b1902caf3d300a4b3c1e8b91d490e543a3d9
Maria Shvets <maria@shvets.eu>
2018-02-07 17:40:42 +0200

    added selectors to filtration of articles

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,10 +1,18 @@
-export function filteredArticleSelector (state) {
-    debugger;
-    const {selected, dateRange: {from, to}} = state.filters;
-    return state.articles.filter(article => {
+import {createSelector} from 'reselect';
+
+export const articlesGetter = state => state.articles;
+export const filtersGetter = state => state.filters;
+
+export const filteredArticleSelector = createSelector (articlesGetter, filtersGetter, (articles, filters) => {
+    const {selected, dateRange: {from, to}} = filters;
+
+    return articles.filter(article => {
         const published = Date.parse(article.date);
+
         return (!selected.length || selected.includes(article.id)) &&
             (!from || !to || (published > from) && (published < to))
     })
-}
+});
+
+
 
